f20d1d618bc2f331f3b38abcd860a228fee52895
Jason Robey <jason.robey@gmail.com>
2013-07-23 23:16:05 -0400

    Controlers are ready

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -16,12 +16,51 @@ angular.module('myApp.controllers', []).
       $scope.name = 'Error!'
     });
 
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
+  }).controller('IndexCtrl', function ($scope, $http) {
+        $http.get('/api/posts').
+            success(function(data, status, headers, config) {
+                $scope.posts = data.posts;
+            });
+    }).controller('AddPostCtrl', function ($scope, $http, $location) {
+        $scope.form = {};
+        $scope.submitPost = function () {
+            $http.post('/api/post', $scope.form).
+                success(function(data) {
+                    $location.path('/');
+                });
+        };
+    }).controller('ReadPostCtrl', function ($scope, $http, $routeParams) {
+        $http.get('/api/post/' + $routeParams.id).
+            success(function(data) {
+                $scope.post = data.post;
+            });
+    }).controller('EditPostCtrl', function ($scope, $http, $location, $routeParams) {
+    $scope.form = {};
+    $http.get('/api/post/' + $routeParams.id).
+        success(function(data) {
+            $scope.form = data.post;
+        });
+        $scope.editPost = function () {
+            $http.put('/api/post/' + $routeParams.id, $scope.form).
+                success(function(data) {
+                    $location.url('/readPost/' + $routeParams.id);
+                });
+        };
+    }).controller('DeletePostCtrl', function ($scope, $http, $location, $routeParams) {
+        $http.get('/api/post/' + $routeParams.id).
+            success(function(data) {
+                $scope.post = data.post;
+            });
 
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
+        $scope.deletePost = function () {
+            $http.delete('/api/post/' + $routeParams.id).
+                success(function(data) {
+                    $location.url('/');
+                });
+        };
 
-  });
+        $scope.home = function () {
+            $location.url('/');
+        };
+    }
+);
